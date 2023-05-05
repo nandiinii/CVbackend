3fd4f51cf25dd0eba525ef6993f59331781bb43e
nandiinii <preethanandini175@gmail.com>
2023-05-05 11:37:32 +0530

    Initial Commit

---
+++ api/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .views import BlacklistTokenView,LoggedInUserView,RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+
+
+router=DefaultRouter()
+router.register('register',RegisterView,basename='register')
+
+
+urlpatterns = [
+    path('',include(router.urls)),
+    path('api/token/',TokenObtainPairView.as_view(),name="token_obtain"),
+    path('api/token/refresh/',TokenRefreshView.as_view(),name="refresh_token"),
+    path('api/token/blacklist/',BlacklistTokenView.as_view(),name="blacklist"),
+    path('current-user/', LoggedInUserView.as_view(), name='currentuser'),
+]
