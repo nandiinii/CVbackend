0e13620778f10d8c297ea548c08e09491cbf589d
nandiinii <preethanandini175@gmail.com>
2023-05-29 21:19:00 +0530

    Added retrieve part

--- training/urls.py
+++ training/urls.py
@@ -8,11 +8,13 @@ router=DefaultRouter()
 my_viewset = PersonalityViewSet.as_view({
     'post': 'predict_personality',
     'create' : 'get',
+    'get' : 'retrieve'
 })
 
 urlpatterns = [
     path('', include(router.urls)),
     path('predict/', my_viewset, name='predict'),
+    path('predict/<int:pk>/', my_viewset, name='predict-detail'),
     path('aptitude/', AptitudeTest.as_view()),
    path('question/<int:pk>',QuestionListView.as_view()),
    path('score/',ScoreView.as_view())
